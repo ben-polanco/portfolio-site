d1862381e8ef75fa26973a71738688f6ebe00795
ben-polanco <benpolanco@icloud.com>
2023-03-28 19:24:24 -0400

    First stable commit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,24 +1,13 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import { FontLoader } from 'next/font'
 
-class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
-  }
-
-render() {
+export default function Document() {
   return (
     <Html lang="en">
-      <FontLoader urls={['/fonts/SpaceMono-Regular.ttf']} />
       <Head />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-    )
-  }
+  )
 }
-
-export default MyDocument
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Space_Mono } from 'next/font/google'
+import '@fontsource/space-mono';
 import styles from '@/styles/Home.module.css'
 import Map from '../components/map'
 
