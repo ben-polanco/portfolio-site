6df639eeccbccc70c11cbfc83938dc6d42866416
ben-polanco <benpolanco@icloud.com>
2023-04-17 18:03:13 -0400

    - [ ] Create content cards (partial)

--- src/components/content-card.js
+++ src/components/content-card.js
@@ -5,17 +5,17 @@ import ReactHtmlParser from 'html-react-parser';
 const ContentCard = ({ experience }) => {
   return (
     <div className={styles.left_content} id={`${experience.name}_content`}>
-      <div className={styles.content_img}>.img placeholder</div>
+      {/*<div className={styles.content_img}>.img placeholder</div>*/}
       <h4>{experience.title}</h4>
-      <h6>Client Name</h6>
+      {/*<h6>Client Name</h6>*/}
       <i id="duration" />
-      <div className={styles.content_icons}>
-        <div className={styles.icons}> icon placeholder
-          <i id="projecttype" />
-          <i id="producttype_analytictool_app_device_etc" />
-          <i id="B2B / C" />
+        {/*<div className={styles.icons}> icon placeholder
+            <div className={styles.content_icons}>
+            <i id="projecttype" />
+            <i id="producttype_analytictool_app_device_etc" />
+            <i id="B2B / C" />
         </div>
-      </div>
+      </div>*/}
       {ReactHtmlParser(experience.content)}
     </div>
   );
