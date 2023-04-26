a4b45f45af247070271e6e3933406f9e7ad1cdf8
ben-polanco <benpolanco@icloud.com>
2023-04-26 17:06:29 -0400

    pointer events  & experience updates

--- src/components/Input/experiences.js
+++ src/components/Input/experiences.js
@@ -5,31 +5,31 @@ const Experiences = () => {
       [{
         "id":1,
         "name":"hospital",
-        "title":"UNLOCKING PERSONALIZED MEDICINE FOR DIABETES",
-        "content":"<h5>Led product innovation team at a leading Pharma Co. focused on increasing consumption of proprietary therapies (insulin & GLP-1s) using personalized medicine as a pull-through mechanism. Worked with medical affairs, regulatory, legal, and the digital transformation CoE to identify viable populations, gauge patient sentiment, and define the north star vision.<br /><br />The product strategy initiative resulted in partnership with a startup pursuing early trials of a novel microfluidics device for early detection of poor kidney function.</h5><br /><p># BACKGROUND:<br />  Diabetes has negative effects on kidney health.  Kidney dialysis is the most expensive complication of diabetes, but chronic kidney disease (CKD) is difficult to diagnose. Kidney damage often goes unchecked until major complications arise.<br /><br />Metformin, the leading 1st line therapy for diabetes, also has negative side effects on kidney function. <b>We hypothesized that increasing early diagnoses or genetic testing for CKD risk would prompt HCPs to move patients from Metformin to less damaging therapies (like my client's insulin or GLP-1s).</b> My client holds a strong market share of existing Metformin alternatives, so we expect a large portion of the increased sales to be captured directly, with knock-on effects for pricing power due to improved long-term outcomes.</p>"
+        "title":"Breaking therapeutic inertia in diabetes",
+        "content":"<h5>Led strategy team at a leading Pharma Co. focused on improving patient compliance by enabling at-home alternatives to traditional point of care services. Worked with medical affairs, legal/regulatory, and the digital transformation CoE to research market dynamics, technology trends, and patient/provider challenges, culminating in a north star vision for digital health at their organization.<br /><br />Alongside the overarching strategy, the initiative resulted in partnership with a startup pursuing early trials of a microfluidics device for early detection of poor kidney function.</h5><br /><p># BACKGROUND:<br />  The diabetes epidemic incurs heavy costs in the health system. End-stage kidney failure (ESRD) is among the most expensive complications of diabetes, but chronic kidney disease (CKD) is difficult to diagnose. Kidney damage often goes unchecked until major complications arise.<br /><br />Metformin, the leading 1st line therapy for diabetes, also has negative side effects on kidney function. <b>We hypothesized that at-home diagnostics or genetic tests for CKD risk would expand early diagnoses and result in increased conversion from Metformin to insulin or GLP-1s.<br /><br />The initiative resulted in partnership with a startup pursuing early trials of a novel microfluidics device for early detection of poor kidney function.</b> My client holds a strong market share of existing Metformin alternatives, so we expect a large portion of the increased sales to be captured directly with knock-on effects for pricing power due to improved long-term outcomes.</p>"
        },{
         "id":2,
         "name":"foundation",
-        "title":"PREDICTING OUTBREAKS OF DENGUE FEVER",
+        "title":"Predicting outbreaks of dengue fever",
         "content":"<h5>Partnered with a global foundation to create the product strategy and market MVP for an early-warning system focused on climate-sensitive disease outbreaks. The product empowers public health decision makers with insight on how to re-allocate critical health resources to mitigate disease burden on vulnerable populations.</h5><br /><p># BACKGROUND:<br />TBC</p>"
       },{
         "id":3,
         "name":"hotel",
-        "title":"BUILDING A SMARTER HOME AWAY FROM HOME",
+        "title":"Building a smarter home away from home",
         "content":"<h5>Managed the redesign & delivery of a new B2C global booking app (Native iPhone & Android) for a luxury hotel group. Special focus on connected room & personalized digital experiences on-property. Launched native iPhone & Android versions in US & China, with additional markets planned.</h5><br /><p># BACKGROUND:<br />  TBC</p>"
       },{
         "id":4,
         "name":"city",
-        "title":"DIGITIZING CITY SERVICES",
+        "title":"Digitizing city services",
         "content":"<h5>Led a product incubation co-venture between Accenture, Huawei, & Ooredoo that focused on creating digital solutions to sustainability & public health challenges in Doha, Qatar.</h5><br /><p># BACKGROUND:<br />  TBC</p>"
       },{
         "id":5,
         "name":"park",
-        "title":"INCREASING COMMUNITY ACCESS TO BROADBAND",
+        "title":"Increasing community access to broadband",
         "content":"<h5>Worked with a community board in Santa Fe, CDMX to strategize & design digital services to drive foot traffic and economic development for a public park. The park was outfitted with broadband, smart lighting, and environmental sensors to act as a platform for streamlining park operations and enabling new digital experiences. Our design strategy laid out a vision for tech-enabled community building and public engagement that could be rolled out over time to elevate the profile of the park.</h5><br /><p># BACKGROUND:<br />  TBC</p>"
       },{
         "id":6,"name":"farm",
-        "title":"VETERINARY TOOLS FOR HEALTHY DAIRY COWS",
+        "title":"Veterinary tools for healthy dairy cows",
         "content":"<h5>Co-led digital products organization of 25+ people for a precision animal health company, inclusive of devices, hubs, and software. Led design & delivery of a precision health tool for veterinarians to care for dairy cows' reproductive health, and reduce calf mortalities by enabling earlier diagnosis & treatment of diahrreal disease in youngstock.</h5><br /><p># BACKGROUND:<br />  TBC.  Stood up an agile digital health factory to scale product design, development, & maintenance capabilities. Partnered with business leads to consolidate legacy products under a single organization, rationalize continued product support, and balance investments in legacy assets vs. product innovation.</p>"
       }]
     );
