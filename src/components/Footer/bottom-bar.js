5f5bdd7e65be5a4eb4a3e35784da68e0299f4122
ben-polanco <benpolanco@icloud.com>
2024-03-09 17:03:08 -0500

    Updated footer text

--- src/components/Footer/bottom-bar.js
+++ src/components/Footer/bottom-bar.js
@@ -5,7 +5,7 @@ import styles from '../../styles/Home.module.css';
 function BottomBar() {
   return (
     <div className={styles.bottombar}>
-      <p><span>©</span> Designed & built by Ben Polanco with React, Sketch, and a bit of TLC</p>
+      <p><span>©</span> Designed & built by Ben Polanco with React and Sketch</p>
     </div>
   );
 }
--- src/components/Footer/info.js
+++ src/components/Footer/info.js
@@ -5,8 +5,8 @@ import styles from '../../styles/Home.module.css';
 function Info() {
   return (
     <div className={styles.info}>
-      <h2>Hire me</h2>
-      <p><span>I enjoy facing intractable problems to find grounded, human solutions that work in the real world.</span>  I&#39;m passionate about improving health, reducing societal costs, and building stronger, more vibrant communities.
+      <h2>Hi</h2>
+      <p><span>I enjoy facing intractable problems to find grounded, human solutions that work in the real world.</span>  I&#39;m passionate about the potential of tech and design to improve our health and build stronger, more vibrant communities.
 <br /><br />
 My product experience centers on <span>B2B and B2C software apps (web, iOS, Android) that use connected devices and advanced analytics.</span> Please feel free to reach out if you&#39;re interested in knowing more about me or my work. I&#39;m always open to new opportunities and challenges.
 <br /><br />
